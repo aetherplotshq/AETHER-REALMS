ada4cf8fda8234686e05c28108679eae3cee6ac4
AETHER <aetherplotshq@gmail.com>
2026-01-28 13:16:45 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
-// Aether Realms Firebase Configuration
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, set, get, update, onValue, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase, ref, set, onValue, push, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDmMgjlpXsk0Ip5Frrlifu6bgRJNNIIYzM",
@@ -16,4 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, set, get, update, onValue, remove };
+export { db, ref, set, onValue, push, remove };
