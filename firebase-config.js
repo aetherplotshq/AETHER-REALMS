e14da410f74414ef767fe7cbdacaa4df028e121f
AETHER <aetherplotshq@gmail.com>
2026-01-31 17:58:30 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, set, onValue, push, remove, update, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase, ref, set, onValue, push, remove, update } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmMgjlpXsk0Ip5Frrlifu6bgRJNNIIYzM",
-  authDomain: "aether-realms.firebaseapp.com",
-  databaseURL: "https://aether-realms-default-rtdb.firebaseio.com",
-  projectId: "aether-realms",
-  storageBucket: "aether-realms.firebasestorage.app",
-  messagingSenderId: "745970771566",
-  appId: "1:745970771566:web:1b0607362279665074a87f",
-  measurementId: "G-XTRFZJTMEW"
+  databaseURL: "https://aether-realms-default-rtdb.firebaseio.com" // আপনার ডাটাবেস ইউআরএল
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-
-export { db, ref, set, onValue, push, remove, update, serverTimestamp };
+export { db, ref, set, onValue, push, remove, update };
